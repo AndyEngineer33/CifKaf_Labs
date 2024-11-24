98eb4f6c4b5d4e3cbaf325aab2380a15cd754703
AndyEngineer33 <163159451+AndyEngineer33@users.noreply.github.com>
2024-11-25 01:17:09 +0300

    Лабораторная 3
    Все лабораторные прошли проверку (см. скриншоты)

---
+++ Лабораторная 3/lab3_task1.py
@@ -0,0 +1,15 @@
+def item_index (products, item_to_find): #Определяем функцию поиска индекса
+    if item_to_find in products:
+        return products.index(item_to_find)
+    else:
+        return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = item_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
